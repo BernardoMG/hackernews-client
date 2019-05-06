387a388ee74427e2d4ce0470a605336ef1a1042b
BernardoMG <bernardo.marquesg@gmail.com>
2019-05-06 11:19:32 +0100

    Updates

---
+++ precache-manifest.81c9a8b43c6f13c3d5481a31fd7af456.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "186a83faf4a391128de13657754b032a",
+    "url": "/hackernews-client/index.html"
+  },
+  {
+    "revision": "ddebb251eb04f8d86b39",
+    "url": "/hackernews-client/static/css/main.15eef4e9.chunk.css"
+  },
+  {
+    "revision": "b9c54d68b68768d1c05c",
+    "url": "/hackernews-client/static/js/2.d9b773c6.chunk.js"
+  },
+  {
+    "revision": "ddebb251eb04f8d86b39",
+    "url": "/hackernews-client/static/js/main.c16b5219.chunk.js"
+  },
+  {
+    "revision": "a2a141c300e4b66981e3",
+    "url": "/hackernews-client/static/js/runtime~main.4645b201.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/hackernews-client/precache-manifest.81c9a8b43c6f13c3d5481a31fd7af456.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/hackernews-client/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
